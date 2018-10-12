0ef001a7f2cf311ed5cf9fadda4dae96d8528bf5
chenkui <chenkui@chkui.com>
2018-10-12 16:06:14 +0800

    add

--- sample/starting/liner_model.py
+++ sample/starting/liner_model.py
@@ -65,15 +65,20 @@ class LinerModel:
         return self
 
     def predict(self, features):
+        """
+
+        :param features:
+        :return:
+        """
         ori = self.__model__(features)
         return ori
 
     def __init_params__(self, feature, label):
         """
         根据输入的特征和标记确定模型的参数形状（shape）
-        :param feature:
-        :param label:
-        :return:
+        :param feature: 输入特征，案例数据形状为（n, 4）
+        :param label: 结论标签，案例数据形状为(n, 2)
+        :return: this
         """
         np_feature = np.array(feature, dtype=np.float64)
         np_label = np.array(label, dtype=np.float64)
@@ -84,11 +89,125 @@ class LinerModel:
         return self
 
     def __model__(self, features):
+        """
+        运算模型。
+        模型的线性表达式为： F(X) = Wij * Xj + Bi。但是实际运算我们都使用矩阵来执行。
+        Wij Xj Bi 以及F(X)都是一个矩阵。根据案例的数据结构，用矩阵计算过程如下：
+
+               Xj          ×      Wij      +      Bi       =        F(X)        i∈[0,2) j∈[0,4)
+
+        [[X1, X2, X3, X4]    [[W11, W21]                       [[F(X)1, F(X)2]    样本1
+         [X1, X2, X3, X4]     [W12, W22]                        [F(X)1, F(X)2]    样本2
+         [X1, X2, X3, X4]  ×  [W13, W23]   +  [[B1, B2]]   =    [F(X)1, F(X)2]    样本3
+         [X1, X2, X3, X4]     [W14, W24]]                       [F(X)1, F(X)2]    样本4
+         ………………………………………]                                       …………………………………]    …………
+
+
+
+        :param features: 特征数据
+        :return:
+        """
         np_feature = np.array(features)
         return np.matmul(np_feature, self.__weight) + self.__bias
 
-    def __optimize_step__(self, features, labels, step=0.1):
+    def __loss__(self, features, labels):
+        """
+        损失函数——差异值的方差：
 
+        LOSS = 1/m∑(Wij * Xj + Bi - Yi)^2 => LOSS = 1/m∑(F(X)ij - Yi)^2
+
+        其中:
+            i是标签分类值的个数，所以i∈[0, len(labels[0]))
+            j是特征的个数，所以j∈[0, len(features[0]))
+            求和公式和方差表示对输入的训练样本进行方差计算之后再全局求和。
+
+        :param features:
+        :param labels:
+        :return:
+        """
+        d_values = self.__model__(features) - labels
+        d_values = d_values.flatten()
+        _len = len(d_values)
+        loss = 0
+        for e in d_values:
+            loss += e * e / _len
+        return loss
+
+    def __optimize_step__(self, features, labels, step=0.1):
+        """
+        优化器：损失函数__loss__对每一个权重参数求偏导。
+        这里使用最基本的梯度递减方法来求每一个权重参数的递减方向，需要对每一个权重参数求偏导：
+
+        已知：F(X)ij = Wij * Xj + Bi; LOSS(X, Y) = F(X)ij - Yi; LOSS = 1/m∑(F(X)ij - Yi)^2。
+        其中m为样本总量，求和公示对所有的特征计算数值和标签数值求方差。对权重Wij求偏导的过程如下：
+
+        展开第一个样本数据的方差：
+
+        LOSS(features[0], labels[0]) = L =
+            1/m × (W11 × X1 + W12 × X2 + …… + W1j × Xj + B1 - Y1)^2 +
+            1/m × (W21 × X1 + W22 × X2 + …… + W2j × Xj + B2 - Y2)^2 +
+            …………
+            1/m × (Wi1 × X1 + Wi2 × X2 + …… + Wij × Xj + Bi - Yi)^2
+
+        1）对W11求偏导 =>
+            ∂L/∂W11 = 2/m(W11 × X1 + W12 × X2 + …… + W1j × Xj + B1 - Y1) × X1 + 0 + ……
+
+        2）对W12求偏导 =>
+            ∂L/∂W12 = 2/m(W11 × X1 + W12 × X2 + …… + W1j × Xj + B1 - Y1) × X2 + 0 + ……
+
+        3）对W)22求偏导 =>
+            ∂L/∂W22 = 2/m(W21 × X1 + W22 × X2 + …… + W1j × Xj + B2 - Y2) × X2 + 0 + ……
+
+        4）所以对Wij求偏导 =>
+            ∂L/∂Wij = 2/m(Wi1 × X1 + Wi2 × X2 + …… + Wij × Xj + Bi - Yi) × Xj + 0 + ……
+
+        5) 因此在整个样本空间的偏导公式为（对m个样本进行求和）：
+
+            Δ(∂L/∂Wij) = 2/m∑(Wi1 × X1 + Wi2 × X2 + …… + Wij × Xj + Bi - Yi) × Xj =>
+            Δ = 2/m∑(k)∑i(j)(Wij × Xj + Bi - Yi) × Xj
+            其中：
+                ∑(k)表示求所有样本的合计：样本量=m => 下标k ∈ [0, m)
+                ∑i(j)表示单个样本第i列的W对应j下标的合计值：i ∈ [0, len(labels[0])), j ∈ [0, len(features[0]))
+
+        所以每一个权重参数的优化算法为：
+            Wij = Wij - η × Δ（η是变动步长，也就是传入的参数step）
+
+        在编码实现实际算法的时候，还是使用矩阵结合求和公式实现算法，这样通过numpy执行会高效很多，代码也相对简洁：
+            观察Δ的表达式：其中的 Wij × Xj + Bi - Yi 就是运算模型减去标签值，所以可以先进行如下的矩阵计算：
+
+               Xj          ×     Wij       +      Bi       -       Yi     =      LOSSi      i∈[0,2) j∈[0,4)
+
+        [[X1, X2, X3, X4]    [[W11, W21]                       [[Y1, Y2]       [[L1, L2]        样本1
+         [X1, X2, X3, X4]     [W12, W22]                        [Y1, Y2]        [L1, L2]        样本2
+         [X1, X2, X3, X4]  ×  [W13, W23]   +  [[B1, B2]]   -    [Y1, Y2]   =    [L1, L2]        样本3
+         [X1, X2, X3, X4]     [W14, W24]]                       [Y1, Y2]        [L1, L2]        样本4
+         ………………………………………]                                       …………………]        …………………}
+
+        矩阵LOSSi可以看做参与计算的常量，简写为L,表示是一个矩阵。下面以求W11为例详细说明求解过程：
+            使用矩阵之后，上面的表达式替换为：
+
+                2/m∑(k)∑i(j)L × Xjk,
+
+                k表示样本空间的下标，k ∈ [0, m)
+
+            对于单个Wij偏导而言，实际上是求矩阵L第i列与Xj的乘积，然后再求和。用X表示样本矩阵， Tx表示X的转置矩阵。 =>
+
+            Tx                     ×         L               x
+
+         (1)  (2) (3) (4) (m)
+        [[X1, X1, X1, X1, ……]           [[L1, L2](1)
+         [X2, X2, X2, X2, ……]      ×     [L1, L2](2)         ×       2/m
+         [X3, X3, X3, X3, ……]            [L1, L2](3)
+         [X4, X4, X4, X4, ……]}           [L1, L2](4)
+                                             …………………](m)
+
+        :param features:
+        :param labels:
+        :param step:
+        :return:
+        """
+
+        # 整个训练样本的规模，就是公式中的m
         size = len(features)
         base = self.__model__(features) - labels
         w = self.__weight
@@ -117,15 +236,6 @@ class LinerModel:
             b[i] = b[i] - step * sum
         self.__bias = b
         return self
-
-    def __loss__(self, features, labels):
-        d_values = self.__model__(features) - labels
-        d_values = d_values.flatten()
-        _len = len(d_values)
-        loss = 0
-        for e in d_values:
-            loss += e * e / _len
-        return loss
 
 
 def normal_person_look(results):
